94c6f3aac9ea96a78f240f2eaec7bcd673423391
Abdulrhman-Bahaa <95930982+Abdulrhman-Bahaa@users.noreply.github.com>
2024-04-29 18:46:35 +0300

    Update pass_auth_system_main.cpp

--- password_authentication_system/pass_auth_system_main.cpp
+++ password_authentication_system/pass_auth_system_main.cpp
@@ -1,16 +1,13 @@
 /**
  ******************************************************************************
- * @mainpage   Password authentication system
+ * @mainpage   Security System
  * @brief      This source file contains the main function of the application
  * @date       2024-3-27
  ******************************************************************************
 */
-#include "pass_auth_system_config.h"
+#include "pass_auth_system_main.h"
 
 /* Macro Declarations -------------------------------------------------------*/
-#define CHARACTER_WIDTH      8
-#define PASSWORD_LENGTH      strlen(PASSWORD)
-#define DISPLAY_START_COLUMN (((128 - (CHARACTER_WIDTH * PASSWORD_LENGTH))) / 2)
 
 /* Variables Definitions ----------------------------------------------------*/ 
 
@@ -18,51 +15,37 @@
 int main(void) {
     Std_ReturnType ret = E_OK;
     ret |= application_initialize();
+    uint8_t* password = NULL, *received_password = NULL:
+    uint8_t allowed_access_attempts = 0, received_char = 0;
+    system_config(&password, &allowed_access_attempts);
 
-    uint8_t password[] = PASSWORD, remaining_chars = PASSWORD_LENGTH, received_password[PASSWORD_LENGTH + 1],
-    received_char = 0, access_attempts = NUMBER_OF_ALLOWED_ATTEMPTS , start_column = DISPLAY_START_COLUMN, clear = 1;
-
-    ret |= ecual_oled_display_bit_mapping(&oled0, epd_bitmap_lock, 128, 64, 0, 0);    
+    uint8_t remaining_access_attempts = allowed_access_attempts, remaining_chars = strlen(password);
+    received_password = (uint8_t*)malloc(strlen(password) + 1);
+    received_password[strlen(password)] = '\0';
     // Do forever
     while(1) {
         // If there are allowed attempts
-        if (access_attempts > 0) { 
+        if (remaining_access_attempts > 0) { 
             // If the required password digits received
             if (0 == remaining_chars) {
-                ret |= ecual_oled_display_clear(&oled0);
+                ret |= pass_received_callback_fun(received_password);
                 // If received password is correct :
-                if (!compare_strings(received_password, password)) {
-                    access_attempts = NUMBER_OF_ALLOWED_ATTEMPTS;
+                if (!strcmp(received_password, password)) {
+                    remaining_access_attempts = allowed_access_attempts;
                     ret |= correct_pass_callback_fun();
                 }
                 // If received password is incorrect :
                 else {
-                    access_attempts--;
+                    remaining_access_attempts--;
                     ret |= incorrect_pass_callback_fun();
                 }
-                remaining_chars = PASSWORD_LENGTH;
-                clear = 1;
-                start_column = DISPLAY_START_COLUMN;
+                remaining_chars = strlen(password);
             }
             else {
-#if defined(KEYPAD_USER_INPUT)
-            	ret |= ecual_keypad_char_read(&keypad0, &received_char);
-#elif defined(SERIAL_TERMINAL_USER_INPUT)
-            	ret |= mcal_usart_receive_data(&received_char);
-#else
-/* Nothing */
-#endif
+                ret |= user_input_fun(&received_char);
                 if (received_char) {
-                    if (1 == clear) {
-                        ret |= ecual_oled_display_clear(&oled0);
-                        clear = 0;
-                    }
-                    else {
-                        /* Nothing */
-                    }
-                    ret |= ecual_oled_display_char_write(&oled0, received_char, CHARACTER_WIDTH, 16,  2, start_column);
-                    received_password[PASSWORD_LENGTH - remaining_chars] = received_char;
-                    start_column += CHARACTER_WIDTH;
+                    ret |= receiving_chars_callback_fun(received_char);
+                    received_password[strlen(password) - remaining_chars] = received_char;
                     remaining_chars--;
                     received_char = 0;
                 }
@@ -75,9 +58,8 @@ int main(void) {
         else {
             // This function must have its own while loop with its breaker condition 
             ret |= limit_exceeded_callback_fun();
-            access_attempts = NUMBER_OF_ALLOWED_ATTEMPTS;
+            remaining_access_attempts = allowed_access_attempts;
 	    }
     }
     return 0;
 }
-    
