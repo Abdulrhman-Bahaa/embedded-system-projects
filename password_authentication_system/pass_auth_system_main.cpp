040462ac0435c5203522035f696f31b2a55257d1
Abdulrhman-Bahaa <95930982+Abdulrhman-Bahaa@users.noreply.github.com>
2024-04-29 20:28:49 +0300

    Update pass_auth_system_main.cpp

--- password_authentication_system/pass_auth_system_main.cpp
+++ password_authentication_system/pass_auth_system_main.cpp
@@ -15,19 +15,19 @@
 int main(void) {
     Std_ReturnType ret = E_OK;
     ret |= application_initialize();
-    uint8_t* password = NULL, *received_password = NULL:
-    uint8_t allowed_access_attempts = 0, received_char = 0;
+    uint8_t* password = NULL;
+    uint8_t allowed_access_attempts = 0, received_digit = 0;
     system_config(&password, &allowed_access_attempts);
 
-    uint8_t remaining_access_attempts = allowed_access_attempts, remaining_chars = strlen(password);
-    received_password = (uint8_t*)malloc(strlen(password) + 1);
+    uint8_t remaining_access_attempts = allowed_access_attempts, remaining_digits = strlen(password);
+    uint8_t* received_password = (uint8_t*)malloc(strlen(password) + 1);
     received_password[strlen(password)] = '\0';
     // Do forever
     while(1) {
         // If there are allowed attempts
         if (remaining_access_attempts > 0) { 
             // If the required password digits received
-            if (0 == remaining_chars) {
+            if (0 == remaining_digits) {
                 ret |= pass_received_callback_fun(received_password);
                 // If received password is correct :
                 if (!strcmp(received_password, password)) {
@@ -39,15 +39,14 @@ int main(void) {
                     remaining_access_attempts--;
                     ret |= incorrect_pass_callback_fun();
                 }
-                remaining_chars = strlen(password);
+                remaining_digits = strlen(password);
             }
             else {
-                ret |= user_input_fun(&received_char);
-                if (received_char) {
-                    ret |= receiving_chars_callback_fun(received_char);
-                    received_password[strlen(password) - remaining_chars] = received_char;
-                    remaining_chars--;
-                    received_char = 0;
+                ret |= take_digit_from_user(&received_digit);
+                if (received_digit) {
+                    received_password[strlen(password) - remaining_digits] = received_digit;
+                    remaining_digits--;
+                    received_digit = 0;
                 }
                 else {
                     /* Nothing */
@@ -59,7 +58,7 @@ int main(void) {
             // This function must have its own while loop with its breaker condition 
             ret |= limit_exceeded_callback_fun();
             remaining_access_attempts = allowed_access_attempts;
-	}
+	    }
     }
     return 0;
 }
