84b6e9563dad45f81623b147c0186bb5d16b96cb
Abdulrhman-Bahaa <95930982+Abdulrhman-Bahaa@users.noreply.github.com>
2024-04-30 14:55:34 +0300

    Update pass_auth_system_config.cpp

--- password_authentication_system/pass_auth_system_config.cpp
+++ password_authentication_system/pass_auth_system_config.cpp
@@ -21,13 +21,13 @@ Std_ReturnType application_initialize(void) {
 }
 
 /* Functions Implementations ------------------------------------------------*/
-// Default pass is "00000", config it if needed
+// Default pass is "00000", config it if needed.
 void system_config(uint8_t** password, uint8_t* allowed_access_attempts) {
     *password = "00000";
     *allowed_access_attempts = 3;
 }
 
-// This function determine how the input will be taken, put the value in "received_digit" pointer;
+// This function determine how the input will be taken, put the value in "received_digit" pointer.
 // The '\r' will be the indicator that the the pass digits are complete, put it in "received_digit" when required.
 // when backspace is received (delete digit), the new digit will be put in the right digit automaticly, leaving "digit_num" with
 // the correct value.
@@ -48,8 +48,9 @@ Std_ReturnType incorrect_pass_callback_fun(void) {
     return ret;
 }
 
-// This function must have its own while loop with its breaker condition 
-// When break from the loop, the system will reset and enable receiving from input again
+// This function will be called if "allowed_access_attempts" are consumed.
+// This function must have its own while loop with its breaker condition.
+// When break from the loop, the system will reset and enable receiving from input again.
 Std_ReturnType limit_exceeded_callback_fun(void) {
     Std_ReturnType ret = E_OK;
     uint8_t keep_in_loop = 1;
