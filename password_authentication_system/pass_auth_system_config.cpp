977acbef25f2226ae44234de375825dd4c821e5b
Abdulrhman-Bahaa <95930982+Abdulrhman-Bahaa@users.noreply.github.com>
2024-04-30 14:34:40 +0300

    Update pass_auth_system_config.cpp

--- password_authentication_system/pass_auth_system_config.cpp
+++ password_authentication_system/pass_auth_system_config.cpp
@@ -11,18 +11,20 @@
 
 /* Macro Declarations -------------------------------------------------------*/
 
-/* Global Variables --------------------------  -------------------------------*/
+/* Global Variables --------------------------  -----------------------------*/
 
 /* Initialization Function --------------------------------------------------*/ 
+// This function will be called only once, suitable for initialization, oled, keypad, etc..
 Std_ReturnType application_initialize(void) {
     Std_ReturnType ret = E_OK;
     return ret;
 }
 
 /* Functions Implementations ------------------------------------------------*/
+// Default pass is "00000", config it if needed
 void system_config(uint8_t** password, uint8_t* allowed_access_attempts) {
-    *password = "123";
-    *allowed_access_attempts = 10;
+    *password = "00000";
+    *allowed_access_attempts = 3;
 }
 
 // This function determine how the input will be taken, put the value in "received_digit" pointer;
@@ -34,15 +36,18 @@ Std_ReturnType take_digit_from_user(uint8_t* received_digit, const uint8_t digit
 
 Std_ReturnType correct_pass_callback_fun(void) {
     Std_ReturnType ret = E_OK;
+    // Action
     return ret;
 }
 
 Std_ReturnType incorrect_pass_callback_fun(void) {
     Std_ReturnType ret = E_OK;
+    // Action
     return ret;
 }
 
 // This function must have its own while loop with its breaker condition 
+// When break from the loop, the system will reset and enable receiving from input again
 Std_ReturnType limit_exceeded_callback_fun(void) {
     Std_ReturnType ret = E_OK;
     uint8_t keep_in_loop = 1;
