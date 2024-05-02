0c49a2c7e032951348039aa594f3eb7d850ab633
Abdulrhman-Bahaa <95930982+Abdulrhman-Bahaa@users.noreply.github.com>
2024-05-02 09:56:29 +0300

    Update pass_auth_system_main.h

--- password_authentication_system/pass_auth_system_main.h
+++ password_authentication_system/pass_auth_system_main.h
@@ -23,6 +23,7 @@
 /* Functions Declarations ---------------------------------------------------*/
 Std_ReturnType application_initialize(void);
 void system_config(uint8_t** password, uint8_t* allowed_access_attempts);
+void change_pass(const uint8_t* new_pass);
 Std_ReturnType take_digit_from_user(uint8_t* received_digit, const uint8_t digit_num);
 Std_ReturnType correct_pass_callback_fun(void);
 Std_ReturnType incorrect_pass_callback_fun(void);
