8441ca07e1395280f02bbdbdf50317e5c9ea485e
Abdulrhman-Bahaa <95930982+Abdulrhman-Bahaa@users.noreply.github.com>
2024-05-03 14:48:34 +0300

    Update pass_auth_system_main.h

--- password_authentication_system/pass_auth_system_main.h
+++ password_authentication_system/pass_auth_system_main.h
@@ -6,15 +6,15 @@
  * @date       2024-4-27
  ******************************************************************************
 */
-#ifndef PASS_AUTH_SYSTEM_MAIN_H
-#define	PASS_AUTH_SYSTEM_MAIN_H
+#ifndef PASS_AUTH_SYSTEM_CONFIG_H
+#define	PASS_AUTH_SYSTEM_CONFIG_H
 
 /* Includes -----------------------------------------------------------------*/
 #include "std/mcal_std_types.h"
 #include <string.h>
-#include <stdlib.h>
 
 /* Macro Declarations -------------------------------------------------------*/
+#define MAX_PASS_DIGITS 20
 
 /* Macro Functions Declarations ---------------------------------------------*/
 
@@ -22,12 +22,15 @@
 
 /* Functions Declarations ---------------------------------------------------*/
 Std_ReturnType application_initialize(void);
-void system_config(uint8_t** password, uint8_t* allowed_access_attempts);
-void change_pass(const uint8_t* new_pass);
 Std_ReturnType take_digit_from_user(uint8_t* received_digit, const uint8_t digit_num);
+Std_ReturnType system_config(void);
+Std_ReturnType first_pass_received_callback_fun(void);
+Std_ReturnType second_pass_matched_callback_fun(void);
+Std_ReturnType second_pass_mismatched_callback_fun(void);
+Std_ReturnType allowed_access_attempts_received_callback_fun(void);
 Std_ReturnType correct_pass_callback_fun(void);
 Std_ReturnType incorrect_pass_callback_fun(void);
 // This function must have its own while loop with its breaker condition 
 Std_ReturnType limit_exceeded_callback_fun(void);
 
-#endif /* PASS_AUTH_SYSTEM_MAIN_H */
+#endif /* PASS_AUTH_SYSTEM_H */
