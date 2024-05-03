b321acd9e7ca7ea56efb10ddeb4336c1208b943e
Abdulrhman-Bahaa <95930982+Abdulrhman-Bahaa@users.noreply.github.com>
2024-05-03 14:53:15 +0300

    Update pass_auth_system_config.cpp

--- password_authentication_system/pass_auth_system_config.cpp
+++ password_authentication_system/pass_auth_system_config.cpp
@@ -9,39 +9,51 @@
 /* Includes -----------------------------------------------------------------*/
 #include "pass_auth_system_config.h"
 
-/* Macro Declarations -------------------------------------------------------*/
-
 /* Global Variables ---------------------------------------------------------*/
-uint8_t pass_length = 0;
 
 /* Initialization Function --------------------------------------------------*/ 
-// This function will be called only once, suitable for initialization, oled, keypad, etc..
 Std_ReturnType application_initialize(void) {
     Std_ReturnType ret = E_OK;
     return ret;
 }
 
 /* Functions Implementations ------------------------------------------------*/
-// Default pass is "00000", config it if needed.
-void system_config(uint8_t** password, uint8_t* allowed_access_attempts) {
-    *password = "00000";
-    *allowed_access_attempts = 3;
-
-    pass_length = strlen(*password);
-}
-
 // This function determine how the input will be taken, put the value in "received_digit" pointer.
 // The '\r' will be the indicator that the the pass digits are complete, put it in "received_digit" when required.
-// when backspace is received (delete digit), the new digit will be put in the right digit automaticly, leaving "digit_num" with
+// when backspace is received (delete digit), the new digit will be put in the right digit automatically, leaving "digit_num" with
 // the correct value.
 Std_ReturnType take_digit_from_user(uint8_t* received_digit, const uint8_t digit_num) {
     Std_ReturnType ret = E_OK;
     return ret;   
 }
 
-Std_ReturnType correct_pass_callback_fun(void) {
+Std_ReturnType first_pass_received_callback_fun(void) {
     Std_ReturnType ret = E_OK;
     // Action
+    return ret;
+}
+
+Std_ReturnType second_pass_matched_callback_fun(void) {
+    Std_ReturnType ret = E_OK;
+     // Action
+    return ret;
+}
+
+Std_ReturnType second_pass_mismatched_callback_fun(void) {
+    Std_ReturnType ret = E_OK;
+     // Action
+    return ret;
+}
+
+Std_ReturnType allowed_access_attempts_received_callback_fun(void) {
+    Std_ReturnType ret = E_OK;
+     // Action
+    return ret;
+}
+
+Std_ReturnType correct_pass_callback_fun(void) {
+    Std_ReturnType ret = E_OK;
+     // Action
     return ret;
 }
 
@@ -56,10 +68,7 @@ Std_ReturnType incorrect_pass_callback_fun(void) {
 // When break from the loop, the system will reset and enable receiving from input again.
 Std_ReturnType limit_exceeded_callback_fun(void) {
     Std_ReturnType ret = E_OK;
-    uint8_t keep_in_loop = 1;
-    while (keep_in_loop) {
-     // Condition to break the loop
+    while (1) {
     }
     return ret;
 }
-
