9b94a79aba68007f06d78bb066a6b72d42d3e253
Abdulrhman Bahaa <abdulrhman.bahaa85@gmail.com>
2025-03-21 23:05:11 +0200

    Update gcs module

--- UAS/firmware/include/gcs.h
+++ UAS/firmware/include/gcs.h
@@ -1,19 +1,20 @@
 /**
  ******************************************************************************
- * @file       gcs.h
+ * @file       uav_gcs.h
  * @author     Abdulrhman Bahaa
  * @brief      This source file contains declarations for the drone gcs
  * @date       2025-03-14
  ******************************************************************************
 */
-#ifndef GCS_H
-#define	GCS_H
+#ifndef UAV_GCS_H
+#define	UAV_GCS_H
 
 /* Includes -----------------------------------------------------------------*/
 #include <Arduino.h>
 
 /* Macro Declarations -------------------------------------------------------*/
 #define DATA_FROM_GCS_ARRAY_SIZE  6
+#define BACK_CALCULATION_ANTI_WINDUP  0
 #define DATA_OVER_SERIAL_IS_READABLE  1
 
 /* Macro Functions Declarations ---------------------------------------------*/
@@ -24,17 +25,16 @@ class PidController {
       float kp, ki, kd, ka, sp;
       float min_roll_actuator_command, max_roll_actuator_command;
       float error, prev_error, accumulated_error;
-      float pid_output, proportional_term, integral_term, derivative_term;
-      float dt, current_time, prev_time; 
+      float raw_pid_output, actual_pid_output, proportional_term, integral_term, derivative_term;
+      float dt, current_time, prev_time, saturated; 
   
       PidController(float kp, float ki, float kd, float ka, float sp,
                     float min_roll_actuator_command, float max_roll_actuator_command);
-      float compute(float pv);
-      int8_t control();
+      void control(float pv);
 };
 
 /* Variables Declarations ---------------------------------------------------*/
-// Spin motors(1)/Stop motors(0), Kp, Ki, Kd, Ka, SP, Min error, Max error
+// Spin motors(1)/Stop motors(0), Kp, Ki, Kd, Ka, SP, 
 extern float data_from_gcs[DATA_FROM_GCS_ARRAY_SIZE];
 
 /* Functions Declarations ---------------------------------------------------*/
@@ -43,4 +43,4 @@ void send_to_gcs(PidController *controller, float euler_angles[3], float motors_
 void receive_from_gcs(PidController *controller, float data_from_gcs[DATA_FROM_GCS_ARRAY_SIZE]);
 void gcs_interface(PidController *controller, float euler_angles[3], float motors_pwm[4], float data_from_gcs[DATA_FROM_GCS_ARRAY_SIZE]);
 
-#endif	/* GCS_H */
+#endif	/* UAV_GCS_H */
