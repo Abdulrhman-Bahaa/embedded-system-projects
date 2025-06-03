e4db891f15e152c25121f56fda8afe8e1266f6c3
Abdulrhman Bahaa <abdulrhman.bahaa85@gmail.com>
2025-06-03 21:40:19 +0300

    Uses constructor member initializer list

--- uas/uav_firmware/include/control.hpp
+++ uas/uav_firmware/include/control.hpp
@@ -14,20 +14,20 @@
 #include <Arduino.h>
 
 /* Macro Declarations -------------------------------------------------------*/
-#define BACK_CALCULATION_ANTI_WINDUP  0
+#define BACK_CALCULATION_ANTI_WINDUP 0
 
 /* Data Type Declarations ---------------------------------------------------*/
 class PidController {
-    public:
-      float kp, ki, kd, ka, sp;
-      float min_roll_actuator_command, max_roll_actuator_command;
-      float error, prev_error, accumulated_error;
-      float raw_pid_output, actual_pid_output, proportional_term, integral_term, derivative_term;
-      float dt, current_time, prev_time, saturated; 
-  
-      PidController(float kp, float ki, float kd, float ka, float sp,
-                    float min_roll_actuator_command, float max_roll_actuator_command);
-      void control(float pv);
+  public:
+    float kp, ki, kd, ka, sp;
+    float min_roll_actuator_command, max_roll_actuator_command;
+    float error, prev_error, accumulated_error;
+    float raw_pid_output, actual_pid_output, proportional_term, integral_term, derivative_term;
+    float dt, current_time, prev_time, saturated;
+
+    PidController(float kp, float ki, float kd, float ka, float sp, float min_roll_actuator_command,
+                  float max_roll_actuator_command);
+    void control(float pv);
 };
 
-#endif    /* UAV_CONTROL_HPP */
+#endif /* UAV_CONTROL_HPP */
--- uas/uav_firmware/src/control.cpp
+++ uas/uav_firmware/src/control.cpp
@@ -9,39 +9,27 @@
 #include "control.hpp"
 
 /* Functions Implementations -------------------------------------------------*/
-PidController::PidController(float kp, float ki, float kd, float ka, float sp,
-    float min_roll_actuator_command, float max_roll_actuator_command) {
-    (this->kp) = kp;
-    (this->ki) = ki;
-    (this->kd) = kd;
-    (this->ka) = ka;
-    (this->sp) = sp;
+PidController::PidController(float kp, float ki, float kd, float ka, float sp, float min_roll_actuator_command,
+                             float max_roll_actuator_command)
+    : kp(kp), ki(ki), kd(kd), ka(ka), sp(sp), min_roll_actuator_command(min_roll_actuator_command),
+      max_roll_actuator_command(max_roll_actuator_command), raw_pid_output(0), actual_pid_output(0), saturated(0),
+      accumulated_error(0) {}
 
-    (this->min_roll_actuator_command) = min_roll_actuator_command;
-    (this->max_roll_actuator_command) = max_roll_actuator_command;
-
-    raw_pid_output = 0;
-    actual_pid_output = 0;
-
-    accumulated_error = 0;
-    saturated = 0;
-}
-
-void PidController::control(float pv) {
+void
+PidController::control(float pv) {
     current_time = millis();
-    dt = (current_time - prev_time) / 1000.0;  // Convert to seconds
+    dt = (current_time - prev_time) / 1000.0; // Convert to seconds
     error = sp - pv;
 
     if (ki > 0) {
-        #if (BACK_CALCULATION_ANTI_WINDUP == 1)
+#if (BACK_CALCULATION_ANTI_WINDUP == 1)
+        accumulated_error += (error * dt);
+#else
+        if ((!saturated) || ((actual_pid_output * error) < 0)) {
             accumulated_error += (error * dt);
-        #else
-            if ((!saturated) || ((actual_pid_output * error) < 0)) {
-                accumulated_error += (error * dt);
-            }
-        #endif
-    }
-    else {
+        }
+#endif
+    } else {
         accumulated_error = 0;
     }
 
@@ -49,22 +37,20 @@ void PidController::control(float pv) {
     derivative_term = (kd * ((error - prev_error) / dt));
     integral_term = (ki * accumulated_error);
 
-    raw_pid_output =  proportional_term + integral_term  + derivative_term;
-    actual_pid_output =  constrain(raw_pid_output, min_roll_actuator_command, max_roll_actuator_command);
+    raw_pid_output = proportional_term + integral_term + derivative_term;
+    actual_pid_output = constrain(raw_pid_output, min_roll_actuator_command, max_roll_actuator_command);
 
+#if (BACK_CALCULATION_ANTI_WINDUP == 1)
+    accumulated_error -= (ka * (raw_pid_output - actual_pid_output));
+#else
+    if ((actual_pid_output == max_roll_actuator_command) || (actual_pid_output == min_roll_actuator_command)) {
+        saturated = 1;
+    } else {
+        saturated = 0;
+    }
+#endif
 
-    #if (BACK_CALCULATION_ANTI_WINDUP == 1)
-        accumulated_error -= (ka * (raw_pid_output - actual_pid_output));
-    #else
-        if ((actual_pid_output == max_roll_actuator_command) || (actual_pid_output == min_roll_actuator_command)) {
-            saturated = 1;
-        }
-        else {
-            saturated = 0;
-        }
-    #endif
-
-    // Time update and error 
+    // Time update and error
     prev_time = current_time;
     prev_error = error;
     current_time = millis();
